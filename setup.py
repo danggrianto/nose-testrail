6751400719af986adb52373ffe6fda72eb88285c
daniel anggrianto <danggrianto@50onred.com>
2015-01-13 15:41:18 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name='nose-testrail',
-    version='0.0.1',
+    version='0.1.0',
     description='Plugin to send test result to testrail',
     author='Daniel Anggrianto',
-    author_email='danggrianto@50onred.com',
+    author_email='d.anggrianto@gmail.com',
     packages=find_packages(exclude=["tests"]),
     entry_points={
         'nose.plugins.0.10': [
